f48abb55d5f6a5c27872a7a282565fad52f1fdc1
khushboo <aggarwal.khushi1501@gmail.com>
2024-01-08 13:24:04 +0530

    branch

--- src/pages/dashboardpage/dashboardpage.jsx
+++ src/pages/dashboardpage/dashboardpage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import ModalPopup from "../../components/ModalPopup/ModalPopup";
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <>
+      <div>
+        <button onClick={handleOpen}>Create</button>
+        <ModalPopup open={open} handleClose={handleClose} />
+      </div>
+    </>
+  );
 }
 
 export default DashboardPage;
