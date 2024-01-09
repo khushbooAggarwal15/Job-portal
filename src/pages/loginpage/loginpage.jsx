74ab2c3f96ad3a5de7c72ca801be7fe19378e9ba
divyansh24 <divyanshjaryal6@gmail.com>
2024-01-09 13:02:44 +0530

    changes yup

--- src/pages/loginpage/loginpage.jsx
+++ src/pages/loginpage/loginpage.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import { LoginForm, PageContainer, InputField,LoginButton} from "../../styles/pages/login";
 import  {useForm,Controller } from "react-hook-form";
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useNavigate } from "react-router";
 
-function LoginPage() {
-const {control,handleSubmit,reset}=useForm();
+
+const schema=yup.object().shape({
+  username:yup.string().required("Username is Required"),
+  password:yup.string().required("Password is required"),
+});
+
+function LoginPage(){
+
+  const navigate = useNavigate();
+const {control,handleSubmit,reset,formState:{errors}}=useForm({resolver:yupResolver(schema)});
   const onSubmit=(data)=>{
     console.log(data)
+    navigate("/dashboard")
     reset();
   }
   return (
@@ -29,6 +41,7 @@ const {control,handleSubmit,reset}=useForm();
             />
           )}
         />
+        <p style={{color:"red",height:"7px",fontStyle:"italic"}}>{errors?.username?.message}</p>
         <Controller
           name="password"
           control={control}
@@ -40,6 +53,7 @@ const {control,handleSubmit,reset}=useForm();
             />
           )}
         />
+        <p style={{color:"red",height:"7px",fontStyle:"italic"}}>{errors?.password?.message}</p>
         <LoginButton type="submit">Login</LoginButton>
         </LoginForm>
       </PageContainer>
