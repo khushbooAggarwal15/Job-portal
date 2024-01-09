09a427a9ee8c8992df77cbb1ae7b106d25a35a21
khushboo <aggarwal.khushi1501@gmail.com>
2024-01-09 13:46:57 +0530

    pull

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import {  NavBar } from '../../styles/components/NavBar'
+import React from "react";
+import { NavBar } from "../NavBar";
 const Header = () => {
   return (
-   <NavBar>
-     <h1> Job-Portal</h1>
-     </NavBar>
-  )
-}
+    <NavBar>
+      <h1> Job-Portal</h1>
+    </NavBar>
+  );
+};
 
-export default Header
+export default Header;
