8263abf5a61ac477c7b7ce28014904259036e1df
divyansh24 <divyanshjaryal6@gmail.com>
2024-01-10 16:03:22 +0530

    Redux Persist

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import{store ,persistor}from "./redux/store.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,22 @@
+
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootreducer.js";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; 
+
+const persistConfig = {
+  key: "FormData",
+  storage, 
+
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
